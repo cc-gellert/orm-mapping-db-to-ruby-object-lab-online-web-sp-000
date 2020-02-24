f5d805d1c7d75aaf5eb8164508452b0e85f7debd
Chloe Gell <gellert.chloe@gmail.com>
2020-02-24 17:19:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
   
   def self.first_student_in_grade_10 
     sql = <<-SQL
-    SELECT * FROM studnets WHERE grade = 10 LIMIT 1 
+    SELECT * FROM students WHERE grade = 10 LIMIT 1 
     SQL
     
     DB[:conn].execute(sql)
