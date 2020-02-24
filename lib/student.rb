b5b14d2c9b3334862b11c238e231378db70208ea
Chloe Gell <gellert.chloe@gmail.com>
2020-02-24 17:18:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,6 +64,14 @@ class Student
     DB[:conn].execute(sql)
   end 
   
+  def all_students_in_grade_X(grade)
+    sql = <<-SQL 
+    SELECT * FROM students WHERE grade = ? 
+    SQL
+    
+    DB[:conn].execute(sql)
+  end 
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
