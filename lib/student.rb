1ed03f333cac4de3fb1d1b7cd94573d2ef4b3f54
Chloe Gell <gellert.chloe@gmail.com>
2020-02-24 17:22:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,7 +45,9 @@ class Student
     SELECT * FROM students WHERE grade < 12 
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map |row| 
+      self.new_from_db(row)
+    end 
   end 
   
   def self.first_X_students_in_grade_10(num)
